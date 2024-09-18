7650d70cffa24b1ab9fb8c89f59779370d822900
PNPTCMilind <m.philar@pnptc.com>
2024-09-18 13:56:59 -0500

    servo edit

--- Code/Server/servo.py
+++ Code/Server/servo.py
@@ -28,20 +28,25 @@ class Servo:
             self.PwmServo.setServoPulse(15, 500 + int((angle + error) / 0.09))
 
 
-# Main program logic follows:
-if __name__ == '__main__':
-    print("Now servos will rotate to 90°.")
-    print("If they have already been at 90°, nothing will be observed.")
-    print("Please keep the program running when installing the servos.")
-    print("After that, you can press ctrl-C to end the program.")
-    pwm = Servo()
-    while True:
-        try:
-            pwm.setServoPwm('0', 90)
-            pwm.setServoPwm('1', 90)
-        except KeyboardInterrupt:
-            print("\nEnd of program")
-            pwm.PwmServo.setPWM(8, 0, 0)
-            pwm.PwmServo.setPWM(9, 0, 0)
+# # Main program logic follows:
+# if __name__ == '__main__':
+#     print("Now servos will rotate to 90°.")
+#     print("If they have already been at 90°, nothing will be observed.")
+#     print("Please keep the program running when installing the servos.")
+#     print("After that, you can press ctrl-C to end the program.")
+#     pwm = Servo()
+#     while True:
+#         try:
+#             pwm.setServoPwm('0', 90)
+#             pwm.setServoPwm('1', 90)
+#         except KeyboardInterrupt:
+#             print("\nEnd of program")
+#             break
 
-            break
+
+if __name__ == '__main__':
+    pwm = Servo()
+    pwm.setServoPwm('0', 90)
+    pwm.setServoPwm('1', 90)
+    pwm.PwmServo.setPWM(8, 0, 0)
+    pwm.PwmServo.setPWM(9, 0, 0)
